a1713ba09a16ff2aaed1688b85ef2ed5887d2db1
joojdev <jvpiovezan2905@gmail.com>
2026-03-03 08:27:55 -0300

    removed some unused imports

--- src/test/java/br/edu/ifsp/list01/Ex07Test.java
+++ src/test/java/br/edu/ifsp/list01/Ex07Test.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class Ex07Test {
     @ParameterizedTest
--- src/test/java/br/edu/ifsp/list01/Ex09Test.java
+++ src/test/java/br/edu/ifsp/list01/Ex09Test.java
@@ -4,11 +4,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class Ex09Test {
-
-
 
     @ParameterizedTest
     @CsvSource(delimiter = '|', textBlock = """
